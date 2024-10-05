54996d58441438f09a4cdad8bfc02fd2ad6716f8
AK47are <3318517070@qq.com>
2024-10-05 10:44:03 +0800

    完成第三章实验第八题。

--- chapter_3/labs/exp3-8.cpp
+++ chapter_3/labs/exp3-8.cpp
@@ -1,34 +1,58 @@
+// NOTE:解决问题必须可以遍历全部元素，实际上栈并太适合解决这个问题。
 #include <iostream>
 #include <string>
 
 #include "../../docs/lib/include/LinkStack.h"
 using namespace std;
 struct Coord {
-  int x;
-  int y;
-  int di;
-  bool operator==(Coord& coord) { return (x == coord.x && y == coord.y); }
-  bool operator!=(Coord& coord) { return operator==(coord); }
+  unsigned x;
+  unsigned y;
+  friend ostream& operator<<(ostream& os, Coord& coord) {
+    os << "(" << coord.x << ", " << coord.y << ") <-";
+    return os;
+  }
   unsigned LeftDiagonal() { return y - x; }
   unsigned RightDiagonal() { return y + x; }
 };
 
-bool IsAttack(Coord left, Coord right) {
-  if (left != right && left.LeftDiagonal() != right.LeftDiagonal() &&
-      left.LeftDiagonal() != right.RightDiagonal())
-    return false;
-  else
-    return true;
+bool IsAttack(LinkStack<Coord> left, Coord right) {
+  // cout << "left: " << left << "\t\t" << "\n";
+  while (left.Size() != 1) {
+    if (left.GetTop().y == right.y ||
+        left.GetTop().LeftDiagonal() == right.LeftDiagonal() ||
+        left.GetTop().RightDiagonal() == right.RightDiagonal())
+      return true;
+    left.Pop();
+  }
+  return false;
 }
 
 int main() {
-  int n;
+  unsigned n, sum = 0;
   cout << "输入皇后个数：";
   cin >> n;
-  LinkStack<Coord> sol{Coord{0, 0, -1}};
+  LinkStack<Coord> sol{Coord{0, n}};
   string status;
-  Coord temp{0, 0, 0};
+  Coord temp{0, 0};
   while (!sol.IsEmpty()) {
-    for (int i = 0; i < n; ++i)
-      if (!IsAttack(sol.GetTop(), temp)) }
+    if (sol.Size() == n + 1) {
+      ++sum;
+      // cout << "找到组合：" << sol << "\n";
+      temp = sol.GetTop();
+      ++temp.y;
+      sol.Pop();
+    }
+    // cout << sol << "\t\t" << "\n";
+    while (IsAttack(sol, temp) && temp.y != n) ++temp.y;
+    if (temp.y == n) {
+      while (sol.GetTop().y == n - 1) sol.Pop();
+      temp = sol.GetTop();
+      ++temp.y;
+      sol.Pop();
+    } else {
+      sol.Push(temp);
+      ++temp.x, temp.y = 0;
+    }
+  }
+  cout << "一共有 " << sum << " 种解法\n";
 }
