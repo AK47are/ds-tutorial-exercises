8bd35b3661ebd24dcea4cf33643d6c824150c2e5
AK47are <54732151+AK47are@users.noreply.github.com>
2025-04-27 10:15:25 +0800

    Delete chapter_02/leetcode directory

--- chapter_02/leetcode/27-移除元素.py
+++
@@ -1,8 +0,0 @@
-# @leet start
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        res = len(nums) - nums.count(val)
-        while nums.count(val) != 0:
-            nums.remove(val)
-        return res
-# @leet end
